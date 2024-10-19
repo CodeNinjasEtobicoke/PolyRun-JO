8cb0e2de8832efae35f510054172ee224a4eab3a
Astroboy <jcarr701@gmail.com>
2024-10-19 13:49:46 -0400

    step 44

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+
+    [Header("Default Speed")]
+    public float speed = 6;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,6 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        transform.position += Vector3.left * speed * Time.deltaTime;
     }
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    [Header("ChallengeObj Game Object")]
+    public GameObject challengeObject;
+    [Header("Default Spawn Delay Time")]
+    public float spawnDelay = 1f;
+    [Header("Default Swpwan Time")]
+    public float spawnTime = 2f;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        InvokeRepeating("InstantiateObjects", spawnDelay, spawnTime);
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    void InstantiateObjects()
+    {
+        Instantiate(challengeObject, transform.position, transform.rotation);
     }
 }
